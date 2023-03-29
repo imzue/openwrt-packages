8cddd651fc969378c6aa3db703a1f37a5718f3e5
Zue <zuegoo@gmail.com>
2023-03-29 16:19:39 +0800

    chore: v2ray-geodata

--- update.sh
+++ update.sh
@@ -5,10 +5,12 @@ rm -rf luci-app-pushbot
 rm -rf luci-app-mosdns
 rm -rf mosdns
 rm -rf v2dat
+rm -rf v2ray-geodata
 
 git clone --depth 1 https://github.com/Hyy2001X/AutoBuild-Packages && mv -n AutoBuild-Packages/luci-app-adguardhome ./; rm -rf AutoBuild-Packages
 git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot
-git clone --depth 1  https://github.com/sbwml/luci-app-mosdns -b v5 package_mosdns && mv -n package_mosdns/luci-app-mosdns ./; mv -n package_mosdns/mosdns ./; mv -n package_mosdns/v2dat ./; rm -rf package_mosdns
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package_mosdns && mv -n package_mosdns/luci-app-mosdns ./; mv -n package_mosdns/mosdns ./; mv -n package_mosdns/v2dat ./; rm -rf package_mosdns
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata
 
 sed -i.bak -e "s#option binpath '/tmp/AdGuardHome/AdGuardHome'#option binpath '/usr/bin/AdGuardHome'#g" -e "s#option workdir '/usr/bin/AdGuardHome'#option workdir '/etc/AdGuardHome'#g" luci-app-adguardhome/root/etc/config/AdGuardHome
 rm luci-app-adguardhome/root/etc/config/AdGuardHome.bak
