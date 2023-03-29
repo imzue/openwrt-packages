2f96c77382ac612f17bbaf5b1caf5958d9fe113f
Zue <zuegoo@gmail.com>
2023-03-29 16:03:54 +0800

    chore: luci-app-pushbot

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+rm -rf luci-app-adguardhome
+rm -rf luci-app-pushbot
+
 git clone --depth 1 https://github.com/Hyy2001X/AutoBuild-Packages && mv -n AutoBuild-Packages/luci-app-adguardhome ./; rm -rf AutoBuild-Packages
+git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot
 
 sed -i.bak -e "s#option binpath '/tmp/AdGuardHome/AdGuardHome'#option binpath '/usr/bin/AdGuardHome'#g" -e "s#option workdir '/usr/bin/AdGuardHome'#option workdir '/etc/AdGuardHome'#g" luci-app-adguardhome/root/etc/config/AdGuardHome
 rm luci-app-adguardhome/root/etc/config/AdGuardHome.bak
+
+
+rm -rf ./*/.git & rm -rf ./*/.gitattributes
+rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
+
+rm -rf create_acl_for_luci.err & rm -rf create_acl_for_luci.ok
+rm -rf create_acl_for_luci.warn
