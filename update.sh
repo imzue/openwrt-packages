fe38a271bd3c51823912fc26525bfe171e9efc28
Zue <zuegoo@gmail.com>
2023-03-30 14:18:54 +0800

    chore: remove useless cmd

--- update.sh
+++ update.sh
@@ -15,6 +15,3 @@ rm luci-app-adguardhome/root/etc/config/AdGuardHome.bak
 
 rm -rf ./*/.git & rm -rf ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
-
-rm -rf create_acl_for_luci.err & rm -rf create_acl_for_luci.ok
-rm -rf create_acl_for_luci.warn
